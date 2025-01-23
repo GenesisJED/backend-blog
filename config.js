d72a28ad5d92033b09dae6e7251e002f98b10fb7
genesis <escalonagenesis97@gmail.com>
2025-01-23 14:07:53 -0400

    add config

---
+++ config.js
@@ -0,0 +1,4 @@
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173"
+
+export const DB_HOST = process.env.DB_HOST || "localhost"
+export const PORT = process.env.PORT || 3000
--- index.js
+++ index.js
@@ -6,7 +6,8 @@ import authRoutes from './routes/auth-route.js';
 import postRoutes from './routes/post-route.js';
 import commentRoutes from './routes/comment-route.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
+import cors from "cors";
+import { FRONTEND_URL, PORT } from './config.js'
 
 dotenv.config();
 
@@ -19,14 +20,19 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
-
 const app = express();
+
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+  })
+);
+
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 3000!');
 });
 
@@ -34,12 +40,6 @@ app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
-
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
